4850d50d3e6be8358781986b2dfdca74ff8d6c3f
Satyam Singh <161092666+SatyamSingh008@users.noreply.github.com>
2025-05-21 06:43:21 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144353
+let accountEmail ="satyamsingh10524@gmail.com"
+var accountPassword ="12345"
+accountCity ="Jaipur"
+let accountnumber;
+
+//accountId = 2// not allowed
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountnumber])
+
+
+
